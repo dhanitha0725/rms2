63c6805f55707937d42a0066f9ea26cf7eacc45f
dhanitha0725 <152945350+dhanitha0725@users.noreply.github.com>
2025-02-28 09:56:42 +0530

    Add room management features with DTO and validation
    Introduces `RoomConfigurationDto` for room details.
    Updates `AddRoomsCommand` to include the new DTO.
    Implements `AddRoomsCommandHandler` for room addition logic, including facility validation and room number generation.
    Adds `AddRoomsCommandValidator` to ensure valid input.
    Enhances `RoomController` with an endpoint for adding rooms.

---
+++ Application/DTOs/FacilityDtos/RoomConfigurationDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Application.DTOs.FacilityDtos
+{
+    public class RoomConfigurationDto
+    {
+        public string RoomType { get; set;}
+        public int Quantity { get; set; }
+        public int Capacity { get; set; }
+        public int NumberOfBeds { get; set; }
+        public string Status { get; set; } = "Available";
+    }
+}
