8c50508567208ff88db128143aa381662bb461da
dhanitha0725 <152945350+dhanitha0725@users.noreply.github.com>
2025-05-02 14:18:40 +0530

    Add UserController for managing customer details
    Introduces a new `UserController` in the `WebAPI.Controllers` namespace.
    This controller handles HTTP requests for customer details, restricted to
    admin users. It utilizes `IMediator` to process a `GetCustomerDetailsQuery`
    and returns appropriate responses based on the success of the operation.

---
+++ WebAPI/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using Application.Features.ManageUsers.GetCustomerDetails;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController(IMediator mediator) : ControllerBase
+    {
+        [Authorize(Roles = "Admin")]
+        [HttpGet("customers")]
+        public async Task<IActionResult> GetCustomerDetails()
+        {
+            var query = new GetCustomerDetailsQuery();
+            var result = await mediator.Send(query);
+            if (!result.IsSuccess)
+            {
+                return BadRequest(new { Error = result.Error.Message });
+            }
+            return Ok(result.Value);
+        }
+    }
+}
